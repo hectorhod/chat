cf6a25b814e0536a549cf7d09aa1f8288f5320b9
Hectorhod <hectorfreitas@hotmail.com>
2021-11-28 22:06:36 -0300

    Alteracao para Tira Duvidas

---
+++ assets/client2.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+    var socket = io.connect("http://localhost:3002");
+    var ready = false;
+ 
+    Load();
+
+    function Load(){
+        var name = $("#nickname").val();
+        var  time = new Date();
+        $("#name").html(name);
+        $("#time").html('Primeiro acesso: ' + time.getHours() + ':' + time.getMinutes());
+        ready = true;
+        socket.emit("join", name);
+    };
+
+    $("#submit").submit(function(e) {
+        e.preventDefault();
+        $("#nick").fadeOut();
+        $("#chat").fadeIn();
+        var name = $("#nickname").val();
+        var time = new Date();
+        $("#name").html(name);
+        $("#time").html('Primeiro acesso: ' + time.getHours() + ':' + time.getMinutes());
+    
+        ready = true;
+        socket.emit("join", name);
+    });
+
+
+    socket.on("update", function(msg) {
+        if (ready) {
+            $('.chat').append('<li class="info">' + msg + '</li>')
+        }
+    });
+
+    socket.on("lista", function(clients){
+        if (ready) {
+            $('.navbar__menu').html("");
+            for(const [key,value] of Object.entries(clients)){
+                $('.navbar__menu').append('<li class="navbar__item">'+ value +'</li>')
+            }
+        }
+    });
+    
+    $("#textarea").keypress(function(e){
+        if(e.which == 13) {
+             var text = $("#textarea").val();
+             $("#textarea").val('');
+             var time = new Date();
+             $(".chat").append('<li class="self"><div class="msg"><span>'
+                          + $("#nickname").val() + ':</span>    <p>' + text + '</p><time>' + 
+                          time.getHours() + ':' + time.getMinutes() + '</time></div></li>');
+             socket.emit("send", text);
+             scrollToBottom();
+        }
+    });
+    
+    socket.on("chat", function(client,msg) {
+        if (ready) {
+           var time = new Date();
+           $(".chat").append('<li class="other"><div class="msg"><span>' + 
+                        client + ':</span><p>' + msg + '</p><time>' + time.getHours() + ':' + 
+                        time.getMinutes() + '</time></div></li>');
+                        scrollToBottom();
+        }
+       });
+    
+    const menu = document.querySelector('#mobile-menu')
+    const menuLink = document.querySelector('.navbar__menu')
+
+    // display mobile menu
+    const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLink.classList.toggle('active')
+    }
+
+    menu.addEventListener('click', mobileMenu);
+
+    function getRandomColor() {
+        var randomColorHaxValues = Math.floor(Math.random()*16777215).toString(16);
+
+        $('.msg spam').css('color',randomColorHaxValues);
+    }
+});
