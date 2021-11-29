78778dae6ee94dc5cef744d2c15dc969055d9e75
Hectorhod <hectorfreitas@hotmail.com>
2021-11-28 22:07:20 -0300

    .

--- servidor.js
+++ servidor.js
@@ -3,21 +3,51 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 const path = require('path');
-const PORT = 3000
+const PORT = 3002
 
 var clients = {};
 
+app.use(express.json())
 app.use(express.static(path.join(__dirname+'/assets')))
 app.use('/jquery',express.static(path.join(__dirname+'/node_modules/jquery/dist')))
 app.use('/socket.io-client',express.static(path.join(__dirname+'/node_modules/socket.io-client/dist')))
+app.set('views', path.join(__dirname, '/assets'));
+app.set('view engine', 'ejs');
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "false");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization, x-acess-token"
+  );
+  next();
+});
+
+var username = ""
+
+app.post('/', (req,res) => {
+  username = req.body.username
+  //console.log(username);
+  res.status(200).json()
+  //io.emit("join", username);
+})
+
+app.get('/', (req,res) => {
+  res.render('index', {username})
+})
+
 
 // Socket.io
 io.on("connection", function (client) {
-    client.on("join", function(name){
-      console.log("Conectado: " + name);
-      clients[client.id] = name;
+    client.on("join", function(username){
+      console.log("Conectado: " + username);
+      clients[client.id] = username;
       client.emit("update", "Você entrou na sala.");
-      client.broadcast.emit("update", name + " entrou na sala.");
+      client.broadcast.emit("update", username + " entrou na sala.");
       io.emit("lista", clients);
     });
   
